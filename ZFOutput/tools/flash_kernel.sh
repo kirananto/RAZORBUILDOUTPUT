93f9a60c5d70f66fcff0ef7149ce19e680bcb2cc
Kiran Anto <kirananto@gmail.com>
2015-08-09 08:12:04 +0530

    ZFOUTPUT : Change Updater-binary and Flash Script
    >Updater binary was not getting executed
    >FlashScript : Its not /dev/block/bootdevice/by-name/boot ..
     /dev/blocl/by-name/boot

--- ZFOutput/tools/flash_kernel.sh
+++ ZFOutput/tools/flash_kernel.sh
@@ -15,7 +15,7 @@
  # Please maintain this if you use this script or any part of it
  #
 cd /tmp/
-/sbin/busybox dd if=/dev/block/bootdevice/by-name/boot of=./boot.img
+/sbin/busybox dd if=/dev/block/by-name/boot of=./boot.img
 ./unpackbootimg -i /tmp/boot.img
 ./mkbootimg --kernel /tmp/bzImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "init=/init pci=noearly loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1 watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0 bootboost=1 androidboot.selinux=permissive"  --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second /tmp/second.img -o /tmp/newboot.img
-/sbin/busybox dd if=/tmp/newboot.img of=/dev/block/bootdevice/by-name/boot
+/sbin/busybox dd if=/tmp/newboot.img of=/dev/block/by-name/boot
